b75e92dc166e57e82033797bed071504d21109a8
Changyue <changyue.chang@mail.utoronto.ca>
2025-04-27 11:49:47 -0400

    changed basis design

--- examples/vector_field.py
+++ examples/vector_field.py
@@ -22,17 +22,26 @@ basis_left = np.tile(np.array([1.0, 0.0]), (n_verts, 1))
 basis_down = np.tile(np.array([0.0, -1.0]), (n_verts, 1))
 
 # point coswise
-y_curve = (1 + np.cos(np.pi * X[:,0])) / (2 * ns)
+y_curve = (np.cos(np.pi * X[:,0])) / (2 * ns)
 basis_cos = np.zeros((n_verts,2))
-basis_cos[:,1] = np.where(X[:,1] >= y_curve, 1.0, -1.0)
+basis_cos[:,1] = y_curve #np.where(X[:,1] >= y_curve, 1.0, -1.0)
 
+
+y_curve = (1 + np.cos(np.pi * X[:,0])) / (2 * ns)
+basis_old = np.zeros((n_verts,2))
+basis_old[:,1] = np.where(X[:,1] >= y_curve, 1.0, -1.0)
 
 ps.init()
-
+ps.set_ground_plane_mode("shadow_only")
 ps_mesh = ps.register_surface_mesh("cantilever beam", X_np, F_np)
 ps_mesh.add_vector_quantity("point right", basis_left, defined_on="vertices", enabled=False)
 ps_mesh.add_vector_quantity("point cos", basis_cos, defined_on="vertices", enabled=False)
 ps_mesh.add_vector_quantity("point down", basis_down, defined_on="vertices", enabled=False)
+
+# what does the basis look like?
+ps_mesh = ps.register_surface_mesh("deformed beam", X_np + basis_cos, F_np)
+
+ps_mesh = ps.register_surface_mesh("deformed beam_old", X_np + basis_old, F_np)
 
 ps.show()
 
