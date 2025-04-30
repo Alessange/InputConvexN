fb8417e7fee0e4eaa26c353585f56bc7f12bbbb4
{Alessange} <{2863933763@qq.com}>
2025-04-29 23:22:31 -0400

    modified the code with skinning U

--- examples/2d_pca_hessian_examples.py
+++ examples/2d_pca_hessian_examples.py
@@ -4,37 +4,7 @@ import numpy as np
 import torch
 import polyscope as ps
 import torch.nn.functional as F
-
-# Create a simple cantilevered beam
-aspect_ratio = 2
-ns =  2
-X,F = igl.triangulated_grid(aspect_ratio*ns+1,ns+1)
-    # per-vertex mass
-M = igl.massmatrix(X,F,igl.MASSMATRIX_TYPE_VORONOI).diagonal()
-M = torch.tensor(M,dtype=torch.float64).unsqueeze(1)
-
-X /= ns
-X[:,0] *= aspect_ratio
-
-X = torch.tensor(X,dtype=torch.float64,requires_grad=False)
-F = torch.tensor(F,dtype=torch.int64)
-
-
-x = X.clone().detach().requires_grad_(True)
-
-fixed = torch.where(X[:, 0] == 0)[0]
-bc = torch.zeros((fixed.shape[0], 2), dtype=torch.float64)
-
-
-xdot = torch.zeros_like(x)
-xprev = x.clone().detach()
-
-dt = 1/66
-t = 0
-g = torch.tensor([0,-9.8],dtype=torch.float64)
-
-lr = 0.005
-
+from skinning_eigenmode_basis import expand_skinning_eigenmodes
 
 def stable_neohookean_elasticity(x, X, F, scale):
 
@@ -75,28 +45,32 @@ def compute_total_energy(xtilde, x):
         x = x.view(xtilde.shape)
     elastic_energy = stable_neohookean_elasticity(x, X, F, scale=dt**2)
     momentum_energy = momentum_potential(x, xtilde, M)
-    total_energy = elastic_energy + momentum_energy 
+    total_energy = 20 * elastic_energy + momentum_energy 
     return total_energy
 
-U = None
-# Perform PCA
 class Transformation:
 
     def __init__(self, U: torch.Tensor):
 
         self.U = U
+        self.U_pinv = torch.linalg.pinv(U)
 
     def to_full(self, q: torch.Tensor) -> torch.Tensor:
 
-        return self.U @ q
+        X_flat = self.U @ q
+        X = X_flat.view(-1, 2)
+        print(X.shape)
+        return X
 
     def to_reduced(self, x: torch.Tensor) -> torch.Tensor:
 
-        return self.U.T @ x
-
-trans = Transformation(U) 
-q = trans.to_reduced(x)
-q = q.clone().detach().requires_grad_(True)
+        if x.ndim == 2:
+            x_flat = x.reshape(-1)
+        else:
+            x_flat = x
+        # project
+        q = self.U_pinv @ x_flat
+        return q
 
 def integrate():
     global t, q, xdot, g, fixed, bc, dt, trans, X
@@ -106,12 +80,9 @@ def integrate():
     # Compute the reduced coordinates
     x = trans.to_full(q) 
     xtilde = x + dt * xdot + dt**2 * g
-
-    q = trans.to_reduced(xtilde) 
-    
     with torch.no_grad():
         xtilde[fixed] = bc + X[fixed]
-
+        
     x_start = x.clone().detach()
     
     #x = xtilde
@@ -134,7 +105,8 @@ def integrate():
 
             q = q - 1.5 * delta_q
             x_new = trans.to_full(q).view_as(x_start)
-
+            x_new[fixed] = bc + X[fixed]
+            q = trans.to_reduced(x_new)
         x = x_new.clone().detach().requires_grad_(True)
     damping = 1
     xdot = ((x - x_start) / dt * damping).detach()
@@ -150,29 +122,93 @@ def integrate():
 #     ps.show()
 
 
+if __name__ == "__main__":
+    # Create a simple cantilevered beam
+    aspect_ratio = 8
+    ns =  8
+    X,F = igl.triangulated_grid(aspect_ratio*ns+1,ns+1)
+    M = igl.massmatrix(X,F,igl.MASSMATRIX_TYPE_VORONOI).diagonal()
+    X[:,0] *= aspect_ratio
+    X /= ns
+    
+    X_np = X.astype(np.float64)
+    F_np = F.astype(np.int32)
+    n_verts = X_np.shape[0]
+
+    # Compute cotangent Laplacian (symmetric)
+    L = igl.cotmatrix(X_np, F_np)
+
+    # Compute the mass matrix (for generalized eigenproblem)
+    M = igl.massmatrix(X_np, F_np, igl.MASSMATRIX_TYPE_VORONOI)
 
 
-ps.init()
-ps.set_give_focus_on_show(True)
-ps_mesh = ps.register_surface_mesh("x", x.detach().numpy(), F.detach().numpy())
-#ps.set_ground_plane_height_factor(0.125, is_relative=False)
+    num_eigen = 7
 
-def callback():
-    global t, x, xdot, xprev
-    if False: #t > 5:
-        t = 0
-        x = trans.to_full(q).clone().detach().requires_grad_(True)
-        xdot = torch.zeros_like(xdot)
-        xprev = trans.to_full(q).view_as(xprev)
-    integrate()
-    x_full = trans.to_full(q)
-    ps_mesh.update_vertex_positions(x_full.detach().numpy())
+    # Solve the generalized eigenvalue problem: Lx = λMx
+    vals, vecs = sp.sparse.linalg.eigsh(-L, k=num_eigen, M=M, sigma=0, which='LM')
 
-ps.set_ground_plane_mode("shadow_only")
-ps.set_user_callback(callback)
+    basis_expanded = expand_skinning_eigenmodes(vecs[:,:5], X_np)
+    M = igl.massmatrix(X,F,igl.MASSMATRIX_TYPE_VORONOI).diagonal()
+    M = torch.tensor(M,dtype=torch.float64).unsqueeze(1)
+    X = torch.tensor(X,dtype=torch.float64,requires_grad=False)
+    F = torch.tensor(F,dtype=torch.int64)
 
-ps.show()
+    x = X.clone().detach().requires_grad_(True)
+
+    fixed = torch.where(X[:, 0] == 0)[0]
+    bc = torch.zeros((fixed.shape[0], 2), dtype=torch.float64)
 
 
+    xdot = torch.zeros_like(x)
+    xprev = x.clone().detach()
+
+    dt = 1/66
+    t = 0
+    g = torch.tensor([0,-9.8],dtype=torch.float64)
+
+    lr = 0.005
 
 
+    # # Define basis fields
+    # basis_left = np.tile([1.0, 0.0], (n_verts, 1))
+    # basis_down = np.tile([0.0, -1.0], (n_verts, 1))
+    # basis_cos1 = np.zeros((n_verts, 2))
+    # basis_cos1[:,1] = (np.cos(np.pi * X_np[:,0])) / (2 * ns)
+    # basis_cos2 = np.zeros((n_verts, 2))
+    # basis_cos2[:,0] = (np.cos(np.pi * 8 * X_np[:,1])) / (2 * ns)
+
+    # fields = [basis_left, basis_down, basis_cos1, basis_cos2]
+    # U = np.stack([f.reshape(2*n_verts) for f in fields], axis=1)
+    # U = torch.tensor(U, dtype=torch.float64)
+    # X_flat = X_np.reshape(2*n_verts)
+
+    # trans = Transformation(U) 
+    # q = np.linalg.lstsq(U, X_flat, rcond=None)[0]
+    # q = torch.tensor(q, dtype=torch.float64)
+    # q = q.clone().detach().requires_grad_(True)
+
+
+    U = torch.tensor(basis_expanded, dtype=torch.float64)
+    trans = Transformation(U)
+
+    q = trans.to_reduced(X) 
+
+    ps.init()
+    ps.set_give_focus_on_show(True)
+    ps_mesh = ps.register_surface_mesh("x", x.detach().numpy(), F.detach().numpy())
+    #ps.set_ground_plane_height_factor(0.125, is_relative=False)
+
+    def callback():
+        global t, x, xdot, xprev
+        if False: #t > 5:
+            t = 0
+            x = trans.to_full(q).clone().detach().requires_grad_(True)
+            xdot = torch.zeros_like(xdot)
+            xprev = trans.to_full(q).view_as(xprev)
+        integrate()
+        x_full = trans.to_full(q)
+        ps_mesh.update_vertex_positions(x_full.detach().numpy())
+
+    ps.set_ground_plane_mode("shadow_only")
+    ps.set_user_callback(callback)
+    ps.show()
