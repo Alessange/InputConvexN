2ee92fe7c17e8546cfc9470b47a12b2f4a997f72
{Alessange} <{2863933763@qq.com}>
2025-05-10 20:37:25 -0400

    model save

--- cantilevered_beam/train.py
+++ cantilevered_beam/train.py
@@ -40,4 +40,5 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = Autoencoder(input_dim=X.shape[1], hidden_dim=500, latent_dim=5)
     trained_model = train(model, loader, num_epochs=50, lr=1e-3, device=device)
+    torch.save(trained_model.state_dict(), "cantilevered_beam/autoencoder.pth")
 
