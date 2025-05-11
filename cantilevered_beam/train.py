752637940149f0b7a48ae5e43e2e49a6dd0a7bfa
{Alessange} <{2863933763@qq.com}>
2025-05-10 21:16:07 -0400

    modified the parameters

--- cantilevered_beam/train.py
+++ cantilevered_beam/train.py
@@ -22,7 +22,7 @@ def train(model, loader, num_epochs=100, lr=0.001, device=None):
             loss = criterion(x_recon, x)
             loss.backward()
             optimizer.step()
-        print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item():.4f}")
+        print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}")
 
     return model
 
@@ -35,10 +35,10 @@ if __name__ == "__main__":
     X = all_disp.view(num_samples, -1).float()
 
     dataset = TensorDataset(X)                       
-    loader  = DataLoader(dataset,batch_size=16,shuffle=True)
+    loader  = DataLoader(dataset,batch_size=32,shuffle=True)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = Autoencoder(input_dim=X.shape[1], hidden_dim=500, latent_dim=5)
-    trained_model = train(model, loader, num_epochs=500, lr=1e-3, device=device)
+    trained_model = train(model, loader, num_epochs=100, lr=1e-3, device=device)
     torch.save(trained_model.state_dict(), "cantilevered_beam/autoencoder.pth")
 
